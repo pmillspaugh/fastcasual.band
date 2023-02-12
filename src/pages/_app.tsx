ff843d7ac314fb884e54c8d2d95892b5c10c7995
Peter Millspaugh <peter@seedfi.com>
2023-02-12 15:38:46 -0500

    Update _app, _document, tsconfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,11 @@
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Righteous } from "@next/font/google";
-
-const righteous = Righteous({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-righteous",
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${righteous.variable} font-sans`}>
+    <Layout>
       <Component {...pageProps} />
-    </div>
+    </Layout>
   );
 }
