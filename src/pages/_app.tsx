49249845885f054f963fb5c4565c532b88b157a9
Peter Millspaugh <peter@seedfi.com>
2023-02-12 12:09:20 -0500

    Install Tailwind and load custom local font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Righteous } from "@next/font/google";
+
+const righteous = Righteous({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-righteous",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${righteous.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
